8d1feffaa62d9ca4205cdba9389c846113cb1df1
Natasya47 <144413336+Natasya47@users.noreply.github.com>
2023-09-08 11:54:03 +0700

    Add files via upload

---
+++ 127-Natasya Haidar Romadhoni.py
@@ -0,0 +1,11 @@
+#Celcius ke Fahrenheit
+Celcius = float(input("Masukkan nilai celcius: "))
+Fahrenheit = (Celcius * 9/5) + 32
+
+print(Celcius, "°C " "Sama dengan:", Fahrenheit, "°F")
+
+#Fahrenheit ke Celcius
+Fahrenheit = float(input("Masukkan nilai Fahrenheit: "))
+Celcius = (Fahrenheit - 32) * 5/9
+
+print(Fahrenheit, "°F " "Sama dengan:", Celcius, "°C")
